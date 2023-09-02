157f67904cb222e4273d378ae6833c5b258984ed
isgandarhasilov <isgandar.hasilov@yahoo.com>
2023-09-02 13:52:21 +0400

    next next commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My first C code\n");
+    printf("My first C code\n");
+    printf("My first C code\n");
+    printf("My first C code\n");
+    printf("My first C code\n");
+    printf("My first C code\n");
+    printf("My first C code\n");
+    return 0;
+}
